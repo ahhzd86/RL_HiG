fe647509aec8111e234fe87c111135f0c83e7f07
ahhzd86 <53471103+ahhzd86@users.noreply.github.com>
2026-02-13 09:57:10 +0100

    Create hidden_bandit.py

---
+++ hidden_bandit.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def bandit(arm):
+    if arm == 1:
+        return np.random.normal(0.0, 1.0)
+    elif arm == 2:
+        return np.random.normal(2.0, 1.0)
+    elif arm == 3:
+        return np.random.normal(1.0, 3.0)
+    else:
+        raise ValueError("Arm must be 1, 2, or 3.")
