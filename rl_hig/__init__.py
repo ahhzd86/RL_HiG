68876cdfdd048bdf1063f6a64cf51e56dfcda0d9
ahhzd86 <53471103+ahhzd86@users.noreply.github.com>
2026-02-18 10:16:14 +0100

    Update __init__.py

--- rl_hig/__init__.py
+++ rl_hig/__init__.py
@@ -1,6 +1,7 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="NonlinearLQR-v0",
-    entry_point="rl_hig.envs.nonlinear_lqr_env:NonlinearLQREnv",
+    id="NonlinearLQR-v0", "InventoryControl-v0"
+    entry_point="rl_hig.envs.nonlinear_lqr_env:NonlinearLQREnv", "rl_hig.envs.inventory_env:InventoryControlEnv"
+
 )
